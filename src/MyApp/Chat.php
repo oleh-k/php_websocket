c275a2e9ab5475d730cd2f59fbd8d8a89578ebec
oleh_ok <ok-ey@hotmail.com>
2024-01-22 12:14:56 +0200

    open ws socket

---
+++ src/MyApp/Chat.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MyApp;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface
+{
+    protected $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage();
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        
+    }
+
+}
